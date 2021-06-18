5a92045a84c0750e6f43f6e82039c8bce4baf43f
Ted Ribeiro <tedribeiro@gmail.com>
2021-06-18 08:34:21 -0300

    feat(front): remove font awesome e adiciona bootstrap

--- front/site-mercado/src/app/app.module.ts
+++ front/site-mercado/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule,
     HttpClientModule
   ],
   providers: [],
