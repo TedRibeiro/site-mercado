224f356a28bf52581ee8f2a4e8953eeb0966920b
Ted Ribeiro <tedribeiro@gmail.com>
2021-06-22 02:11:59 -0300

    feat(front): adiciona metodo para login

--- front/site-mercado/src/app/services/authentication.service.ts
+++ front/site-mercado/src/app/services/authentication.service.ts
@@ -1,6 +1,11 @@
+import { UserContextService } from './user-context.service';
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+const API = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +13,27 @@ import { Observable, of } from 'rxjs';
 export class AuthenticationService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private userContext: UserContextService
   ) { }
 
-  signIn(userName: string, password: string): Observable<boolean> {
-    return of(true);
+  signIn(username: string, password: string): Observable<AuthResult> {
+    return this.http.post<AuthResult>(`${API}/auth`, {
+      username,
+      password
+    })
+    .pipe(
+      tap(res => this.userContext.signIn(res.token))
+    );
   }
 
   signOut(): Observable<boolean> {
     return of(true);
   }
+}
+
+interface AuthResult {
+  success: boolean,
+  error: string,
+  token: string
 }
